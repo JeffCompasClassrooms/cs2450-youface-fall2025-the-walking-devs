2d1e7f708eb96dc3f52980bab2800c9751af9cc1
Bailey Speakman <baileyspeakman@Baileys-MacBook-Pro-2.local>
2025-11-12 10:16:44 -0700

    changed last line in app.py

--- app.py
+++ app.py
@@ -133,6 +133,7 @@ def logout():
     session.clear()
     return redirect(url_for('login_page'))
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005, debug=False, use_reloader=False,
+threaded=True)
 
